3a9aacf82183d424d68a3648a342832936196ce9
Damiasin <damian.wal@interia.pl>
2022-05-20 13:07:55 +0200

    OK

---
+++ CryptoAPI/Modules/UserContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CryptoAPI.Modules
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<UserDto> Users { get; set; }
+    }
+}
---
+++ CryptoAPI/Startup.cs
@@ -0,0 +1,55 @@
+using CryptoAPI.Modules;
+using CryptoAPI.Repositories;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace CryptoAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get;}
+     // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddScoped<ICryptoRepository, CryptoRepository>();
+            services.AddDbContext<CryptoContext>(options =>
+          options.UseSqlServer(Configuration.GetConnectionString("ConnectionStr")));
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CryptoAPI", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CryptoAPI v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
